4070139ea993ce507a1862ee5af6255974f36108
Földi Balázs <fbalu8@gmail.com>
2024-03-22 13:44:37 +0100

    pair() és pairing()

--- Logarlec/src/logarlecTheGame/Model/Student.java
+++ Logarlec/src/logarlecTheGame/Model/Student.java
@@ -4,8 +4,8 @@ public class Student extends Player implements StudentProtection, PutDown, Pairi
 public
     /**
      * A hallgatót próbálja megölni
-     * @param i Az az item, amire megpróbálja meghívni a függvény a protectSP() függvényt
-     * @return Hamis, ha sikerült valamelyik tárgynak megvédenie a hallgatót, ellenkező esetben igaz
+     * @param i     Az az item, amire megpróbálja meghívni a függvény a protectSP() függvényt
+     * @return      Hamis, ha sikerült valamelyik tárgynak megvédenie a hallgatót, ellenkező esetben igaz
      */
     boolean die(){
         for (Item item : ItemList) {
@@ -17,7 +17,7 @@ public
 
     /**
      * A hallgatót próbálja stunnolni
-     * @return Hamis, ha sikerült valamelyik tárgynak megvédenie a hallgatót, ellenkező esetben igaz
+     * @return      Hamis, ha sikerült valamelyik tárgynak megvédenie a hallgatót, ellenkező esetben igaz
      */
     boolean stun(){
         for (Item item : ItemList) {
@@ -29,13 +29,38 @@ public
 
     /**
      * Tárgyak letételét/használatát kezdeményezi
-     * @param i Az a tárgy amivel szeretnénk dolgozni
+     * @param i     Az a tárgy amivel szeretnénk dolgozni
      */
     void PutDown(Item i){
         i.acceptPutDown(this);
     }
 
-    
-    boolean pairing(Transistor t1, Transistor t2){}
+    /**
+     * Két tranziszto párosítását kezdeményezi
+     * @param t1    Első tranzisztor
+     * @param t2    Második tranzisztor
+     * @return      Ha sikerült párosítani, akkor igazzal tér vissza, különben hamis.
+     */
+    boolean pairing(Transistor t1, Transistor t2){
+        if(t1.acceptPairing(this, t2))
+            return true;
+        return false;
+    }
+
+    /**
+     * Két tranzisztor párosítását végzi
+     * @param t1    Első tranzisztor
+     * @param t2    Második tranzisztor
+     * @return      Ha sikerült párosítani, akkor igazzal tér vissza, különben hamis.
+     */
+    boolean pair(Transistor t1, Transistor t2){
+        if(t2.makePair(t1)){
+            if(t1.makePair(t2)){
+                return true;
+            }
+        }
+        return false;
+    }
+
     boolean protect(Item i){}
 }
