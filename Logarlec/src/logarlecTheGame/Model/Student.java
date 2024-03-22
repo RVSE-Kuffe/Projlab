48bd5bdbfc027f092954b5ba697b717ac81192d9
Földi Balázs <fbalu8@gmail.com>
2024-03-22 13:22:17 +0100

    die(), stun(), putdown()

--- Logarlec/src/logarlecTheGame/Model/Student.java
+++ Logarlec/src/logarlecTheGame/Model/Student.java
@@ -1,10 +1,41 @@
 package logarlecTheGame.Model;
 
 public class Student extends Player implements StudentProtection, PutDown, Pairing {
-public:
-    boolean die(){}
-    boolean stun(){}
-    void PutDown(Item i){}
+public
+    /**
+     * A hallgatót próbálja megölni
+     * @param i Az az item, amire megpróbálja meghívni a függvény a protectSP() függvényt
+     * @return Hamis, ha sikerült valamelyik tárgynak megvédenie a hallgatót, ellenkező esetben igaz
+     */
+    boolean die(){
+        for (Item item : ItemList) {
+            if(item.acceptSP(this))
+                return false;
+        }
+        return true;
+    }
+
+    /**
+     * A hallgatót próbálja stunnolni
+     * @return Hamis, ha sikerült valamelyik tárgynak megvédenie a hallgatót, ellenkező esetben igaz
+     */
+    boolean stun(){
+        for (Item item : ItemList) {
+            if(item.acceptGasProtect(this))
+                return true;
+        }
+        return false
+    }
+
+    /**
+     * Tárgyak letételét/használatát kezdeményezi
+     * @param i Az a tárgy amivel szeretnénk dolgozni
+     */
+    void PutDown(Item i){
+        i.acceptPutDown(this);
+    }
+
+    
     boolean pairing(Transistor t1, Transistor t2){}
     boolean protect(Item i){}
 }
