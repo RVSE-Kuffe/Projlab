4a6403ae1d34081fe7c5770604030627ea5e8c36
Fodor Attila <afodor998@gmail.com>
2024-03-24 10:26:44 +0100

    fix

--- Logarlec/src/logarlecTheGame/Model/Board.java
+++ Logarlec/src/logarlecTheGame/Model/Board.java
@@ -13,27 +13,32 @@ public class Board {
         sk = s;
         sk.names.put(this, n);
     }
-public
-    void init(){System.out.println(sk.names.get(this) + "init");}
 
-    void gameOver(){System.out.println(sk.names.get(this) + "gameOver");}
+    public void init(){System.out.println(sk.names.get(this) + "init");}
 
-    void modify(){System.out.println(sk.names.get(this) + "modify");}
+    public void gameOver(){System.out.println(sk.names.get(this) + "gameOver");}
 
-    void addRoom(Room r1){
+    public void modify(){System.out.println(sk.names.get(this) + "modify");}
+
+    public void addRoom(Room r1){
         roomList.add(r1);
         System.out.println(sk.names.get(this) + "addRoom");
 
     }
 
-    void removeRoom(Room r1){
+    public void removeRoom(Room r1){
         roomList.remove(r1);
         System.out.println(sk.names.get(this) + "removeRoom");
     }
-    boolean forceMerge(Room r1, Room r2){System.out.println(sk.names.get(this) + "forceMerge");return true;}
-    
-    void forcesplit(Room r){System.out.println(sk.names.get(this) + "forceSplit");}
+    public boolean forceMerge(Room r1, Room r2){System.out.println(sk.names.get(this) + "forceMerge");return true;}
 
-    void win(){System.out.println(sk.names.get(this) + "win");}
+    public void forceSplit(Room r){
+        System.out.println(sk.names.get(this) + "forceSplit");
+        Room newRoom= r.newRoom();
+    
+        addRoom(newRoom);
+    }
+
+    public void win(){System.out.println(sk.names.get(this) + "win");}
 
 }
