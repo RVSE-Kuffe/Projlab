1b71f5147e2f79c474096edacabe22fd1e67e1f5
Mikola Bálint István <mikola.balint.istvan@gmail.com>
2024-04-19 21:38:14 +0200

    logikai vaz

--- Logarlec/src/logarlecTheGame/App.java
+++ Logarlec/src/logarlecTheGame/App.java
@@ -1,10 +1,39 @@
 package logarlecTheGame;
 import logarlecTheGame.Skeleton.*;
+import java.util.Scanner;
+
 public class App {
 
-    static boolean toConsole = true
+    static boolean toConsole = true;
+    static char chooser = 'd';
+    static Scanner scanner = new Scanner(System.in);
+
+    static void testfv(){
+        if(toConsole){
+            System.out.println("A konzolra kell irnom");
+        }
+        else{
+            //fileba kell irnia
+        }
+    }
 
     public static void main(String[] args) throws Exception {
-        
+        while(chooser != 'c'){
+            System.out.println("Valassz modszert");
+            System.out.println("Tesztek beolvasasa");
+            System.out.println("Jatek vezerlese parancsokkal");
+
+            chooser = scanner.next().charAt(0);
+
+            if(chooser == 'a'){
+                toConsole = false;
+                System.out.println("Valassz teszteket");
+                System.out.println("...tesztek");
+            }
+            else if(chooser == 'b'){
+                toConsole = true;
+                System.out.println("Parancsolvaso es parseolo logika helye");
+            }
+        }
     }
 }
