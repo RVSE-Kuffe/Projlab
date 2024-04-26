dc15589f4c6a7fe60689d9c97581e689d8d50a92
Földi Balázs <fbalu8@gmail.com>
2024-04-26 20:12:49 +0200

    torlofix

--- Logarlec/src/logarlecTheGame/Model/Item/Tablatorlo.java
+++ Logarlec/src/logarlecTheGame/Model/Item/Tablatorlo.java
@@ -1,23 +1,15 @@
 package logarlecTheGame.Model.Item;
-import java.util.ArrayList;
-import java.util.List;
-import logarlecTheGame.*;
-import logarlecTheGame.Model.Player;
-import logarlecTheGame.Model.Student;
-import logarlecTheGame.Model.Interfaces.CycleBased;
-import logarlecTheGame.Model.Item.Item;
-import logarlecTheGame.Skeleton.*;
+
+import logarlecTheGame.Model.*;
+import logarlecTheGame.Model.Interfaces.*;
 
 public class Tablatorlo extends Item implements CycleBased {
     private int durability;
-    private Skeleton sk;
 
     /**
     *Tablatorlo konstruktor, inicializálja az attribútumokat
      */
-    public Tablatorlo(Skeleton s, String n, int durab) {
-        sk = s;
-        sk.names.put(this, n);
+    public Tablatorlo(int durab) {
         durability=durab;
     }
     
@@ -27,9 +19,7 @@ public class Tablatorlo extends Item implements CycleBased {
      */
     @Override
     public void acceptPutDown(Student s){
-        System.out.println(sk.names.get(this) + "acceptPutDown");
         s.use(this);
-        System.out.println(sk.names.get(this) + "acceptPutDown returned with void");
     } 
     /**
      *Csökkenti a tárgy használhatóságát, 
