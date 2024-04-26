9edd212ba1415d60d539cebc78fca11a5c58635b
Földi Balázs <fbalu8@gmail.com>
2024-04-26 20:56:33 +0200

    cycuse

--- Logarlec/src/logarlecTheGame/Model/CycleUsage.java
+++ Logarlec/src/logarlecTheGame/Model/CycleUsage.java
@@ -4,31 +4,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 import logarlecTheGame.Model.Interfaces.CycleBased;
-import logarlecTheGame.Skeleton.*;
 
 public class CycleUsage {
     private int cycleNumber;
     private List<CycleBased> cbList;
-    private Skeleton sk;
-/**
-  * CycleUsage konstruktor, 
-  *inicializálja az attribútumokat
+    /**
+    * CycleUsage konstruktor, 
+    *inicializálja az attribútumokat
      */
-    public CycleUsage(Skeleton s, String n){
-        sk = s;
-        sk.names.put(this, n);
+    public CycleUsage(){
         cycleNumber = 0;
         cbList = new ArrayList<>();
     }
+
     /**
     * Megsemmisíti az adott tárgyat ami CycleBased-t valósít meg
     *@param c a CycleBased tárgy, vagy elem
      */
     public void destroy(CycleBased c){
-        System.out.println(sk.names.get(this) + " destroy(" + sk.names.get(c) + ")" );
-
-        System.out.println(sk.names.get(this) + " return destroy");
-        return;
+        /*TODO*/
     }
 
      /**
@@ -36,9 +30,6 @@ public class CycleUsage {
     *@param c a CycleBased tárgy, vagy elem
      */
     public void add(CycleUsage c){
-        System.out.println(sk.names.get(this) + " add(" + sk.names.get(c) + ")");
-
-        System.out.println(sk.names.get(this) + " return add");
-        return;
+        /*TODO */
     }
 }
