93fd04f8c6569fc438b668e7bb1eae5159850584
Fodor Attila <afodor998@gmail.com>
2024-05-14 11:21:47 +0200

    boardPanel

--- Logarlec/src/logarlecTheGame/View/BoardPanel.java
+++ Logarlec/src/logarlecTheGame/View/BoardPanel.java
@@ -7,62 +7,72 @@ import logarlecTheGame.Model.*;
 import java.awt.*;
 
 
+
 public class BoardPanel extends JPanel {
+    private Student currentPlayer;
+    private Board board;
 
-    Player player;
-    Board board;
+    public BoardPanel(Student currentPlayer, Board board) {
+        this.currentPlayer = currentPlayer;
+        this.board = board;
+        // Azonnal kirajzolunk valamit
+    }
 
-    public BoardPanel() {
-        setSize(400, 400);
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        paintBoard(g);
+    }
 
-        JPanel panel = new JPanel(null);
-        panel.setBackground(Color.YELLOW);
+    private void paintBoard(Graphics g) {
+        int rows = 4;
+        int cols = 4;
+        int circleSize = 70;
+        int gapy = 10;
+        int gapx=25;
 
-        // Az eltolás a körök bal felső negyedében történik
-        int offsetX = 150;
-        int offsetY = 20;
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                int x = col * (circleSize + gapx);
+                int y = row * (circleSize + gapy);
 
-        final int[] n = new int[]{1};
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++) {
-                JPanel circlePanel = new JPanel() {
-                    @Override
-                    protected void paintComponent(Graphics g) {
-                        super.paintComponent(g);
-                        String roomname = board.objectToString(player.getLocation());
-                        if(roomname.equals("R"+n)) 
-                            g.setColor(Color.BLUE);
-                        else 
-                            g.setColor(Color.GREEN);
-                        g.fillOval(5, 5, getWidth() - 10, getHeight() - 10);
-                        g.setColor(Color.BLACK);
-                        g.drawOval(5, 5, getWidth() - 10, getHeight() - 10);
-                        g.drawString("R"+n[0], getWidth() / 2 - 10, getHeight() / 2 + 5);
-                        
-                    }
-                    
-                    @Override
-                    public Dimension getPreferredSize() {
-                        return new Dimension(80, 80);
-                    }
-                };
-                circlePanel.setBackground(Color.YELLOW);
-                // A körök pozíciója az eltolással
-                int x = j * 100 + offsetX;
-                int y = i * 100 + offsetY;
-                circlePanel.setBounds(x, y, 80, 80);
-                panel.add(circlePanel);
-                n[0]++;
+                // Draw circle
+                g.setColor(getCircleColor(row, col));
+                g.fillOval(x, y, circleSize, circleSize);
+
+                // Draw room name
+                String roomName = getRoomName(row, col);
+                g.setColor(Color.BLACK);
+                g.drawString(roomName, x + circleSize / 4, y + circleSize / 2);
             }
-            
         }
-
-        add(panel);
     }
 
-    public void update(Player p, Board b){
-        player = p;
-        board = b;
+    private String getRoomName(int row, int col) {
+        int index = row * 4 + col;
+        if (index < board.rooms().size()) {
+            Room room = board.rooms().get(index);
+            return board.objectToString(room);
+        }
+        return "";
     }
 
+    private Color getCircleColor(int row, int col) {
+        int index = row * 4 + col;
+        if (index < board.rooms().size()) {
+            Room room = board.rooms().get(index);
+            if (currentPlayer.getLocation() == room) {
+                return Color.BLUE;
+            } else {
+                return Color.GREEN; // Egyéb szobák zöld
+            }
+        }
+        return Color.RED; // Ha a roomList-nél nagyobb az index, akkor piros
+    }
+
+    public void update(Student currentPlayer, Board board) {
+        this.currentPlayer = currentPlayer;
+        this.board = board;
+        repaint(); // Újrarajzoljuk a panelt
+    }
 }
