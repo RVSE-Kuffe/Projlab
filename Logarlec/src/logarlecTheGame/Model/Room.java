1a52ff2953ab95c8ffdcd06108ea82048121d214
Fodor Dávid <dfodor999@gmail.com>
2024-05-20 16:34:16 +0200

    room kommentek

--- Logarlec/src/logarlecTheGame/Model/Room.java
+++ Logarlec/src/logarlecTheGame/Model/Room.java
@@ -5,6 +5,12 @@ import java.util.ArrayList;
 import java.util.List;
 import logarlecTheGame.Model.Item.*;
 
+
+/**
+ * A szoba osztály reprezentálja a játékban szereplő szobákat.
+ * Minden szoba tartalmaz ajtókat, játékosokat és tárgyakat.
+ * Egy szoba lehet gázos, tablatorlos, és lehet ragacsos.
+ */
 public class Room implements Serializable{
     protected List<Door> doorList = new ArrayList<>();
     protected List<Player> playerList = new ArrayList<>();
@@ -18,17 +24,30 @@ public class Room implements Serializable{
     protected int splitCounter = 0;
 
     /**
-     * Szoba osztály konstruktora
-     * beállítja a megfelelő attribútumokat
+     * Szoba osztály konstruktora.
+     * Beállítja a megfelelő attribútumokat.
+     * @param rid A szoba azonosítója.
+     * @param cap A szoba befogadóképessége.
+     * @param b A tábla, amelyhez a szoba tartozik.
      */
     public Room(int rid, int cap, Board b) {
         roomid=rid;
         capacity=cap;
         myBoard = b;
     }
+
+    /**
+     * Visszaadja a szoba befogadóképességét.
+     * @return A szoba befogadóképessége.
+     */
     public int getCapacity(){
         return capacity;
     }
+
+    /**
+     * Visszaadja a szobában lévő játékosok számát.
+     * @return A szobában lévő játékosok száma.
+     */
     public int getPlayerCount(){
         return playerList.size();
     }
@@ -64,6 +83,11 @@ public class Room implements Serializable{
         //ha ragacsos vagy valami akkor removeItem, tehár törlődik, nem lezs a játékban többet
     }
 
+
+    /**
+ * Törli a megadott ajtót a szobából és a tábláról.
+ * @param d Az eltávolítandó ajtó.
+ */
     public void removeDoor(Door d){
         doorList.remove(d);
         myBoard.removeFromMap(d);
@@ -96,6 +120,12 @@ public class Room implements Serializable{
         return false;
     }
 
+    /**
+     * Két szoba egyesítését végzi.
+     * Áthelyezi az itemeket és a játékosokat az első szobából a másodikba.
+     * @param r1 Egyik szoba.
+     * @return true, ha az egyesítés sikeres, különben false.
+     */
     public boolean mergeRoom(Room r1){
         for(Item i:itemList ){
             r1.addItem(i);
@@ -118,6 +148,11 @@ public class Room implements Serializable{
         doorList.add(d);
     }
 
+    /**
+ * Megöli a megadott játékost, eltávolítja a tábláról és a szobából.
+ * @param p Az megölendő játékos.
+ * @return Igaz, ha sikeresen meghalt a játékos
+ */
     public boolean killPlayer(Player p){
         myBoard.removeFromMap(p);
         playerList.remove(p);
@@ -185,6 +220,9 @@ public class Room implements Serializable{
         }
     }
 
+    /**
+ * Megszünteti a szobában lévő gázt.
+ */
     public void makeUnGassed(){
         gassed=false;
     }
@@ -212,10 +250,24 @@ public class Room implements Serializable{
             }
         }
     }
+
+    /**
+ * Elfogadja a szoba párosítását 
+ * 
+ * @param b A tábla, amelyen a párosítást elvégzi.
+ * @param r2 A másik szoba, amelyet ezzel a szobával párosít.
+ * @return Igaz, ha sikerült a párosítás, különben hamis.
+ */
     public boolean acceptPairing(Board b,Room r2){
         return b.pair(this,r2);
     }
 
+
+    /**
+ * Kiküld mindenkit a szobából
+ * 
+ * @param j A takarító
+ */
     public void sendOut(Janitor j) {
         for (int i = 0; i < doorList.size(); i++) { // Az "i" változó érvényessége itt kezdődik
             for (int p = 0; p < playerList.size(); p++) { // A "p" változó érvényessége itt kezdődik
@@ -229,10 +281,23 @@ public class Room implements Serializable{
         }
     }
 
-    public void resetStickyCount() {//stickycount nullazasra van, ha belép a takarito
+    /**
+ * Visszaállítja a ragacsos jelzőszámlálót, ha a takarító belép a szobába.
+ */
+    public void resetStickyCount() {
         this.stickyCount=0;
     }
 
+
+    /**
+ * Visszaadja a szoba adatait, beleértve a játékosokat, a tárgyakat és az attribútumokat.
+ * 
+ * @param b A tábla
+ * @param withPlayers Ha igaz, akkor a játékosokat is listázza.
+ * @param withItems Ha igaz, akkor a tárgyakat is listázza.
+ * @param withAttribs Ha igaz, akkor a attribútumokat is listázza.
+ * @return A szoba adatait tartalmazó szöveg.
+ */
     public String listMe(Board b, boolean withPlayers, boolean withItems, boolean withAttribs){
         StringBuilder returnString = new StringBuilder(b.objectToString(this) + ": ");
         if(withPlayers){
@@ -259,13 +324,27 @@ public class Room implements Serializable{
         return returnString.toString();
     }
 
+    /**
+ * Visszaadja a szobában található ajtókat.
+ * 
+ * @return A szobában található ajtók listája.
+ */
     public List<Door> doors(){
         return doorList;
     }
 
+    /**
+ * Visszaadja a szobában található elemeket.
+ * 
+ * @return A szobában található elemek listája.
+ */
     public List<Item> items(){
         return itemList;
     }
+
+    /**
+ * Beállítja a ragacsos jelzőszámlálót 420-ra.
+ */
     public void makeSticky() {
         stickyCount = 420;
     }
